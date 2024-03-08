677faf251f604011a4b0fd0b8c92175592b2138e
Laura Lavínia <lauralavinia60@gmail.com>
2024-03-07 22:56:25 -0300

    primeiro commit

---
+++ src/app/cep/[id]/page.js
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react"
+import Link from "next/link"
+
+export default function Page({params}) {
+
+    console.log(params.id)
+    const [data, setData] =  useState({})
+
+    useEffect(() => {
+        fetch(`https://viacep.com.br/ws/${params.id}/json/`)
+        .then((res) => res.json)
+        .then((data) => setData(data))
+        .catch((error) => {
+            console.log("Erro na requisição:" , error)
+            setData({error: "CEP não válido"})
+        })
+    }, [params.id])
+
+    console.log(data)
+
+    if(data.erro || data.error){
+        return(
+            <div className="flex flex-col justify-center h-screen bg-[#1f2937] text-white font-bold items-center gap-4">
+                <h1>CEP não encontrado</h1>
+                <Link href="/" className="bg-blue-700 px-8 py-3 round-lg font-bold text-white text-lg
+                houer: bg-blue-800 transition-all duration-300 ease-in-out">Buscar outro CEP</Link>
+            </div>
+        )
+    }
+
+    return (
+        <main className="flex flex-col justify-center items-center h-screen bg-[#1f2937] text-white font-bold">
+            <div className="text-2xl">
+                <h1>CEP:{params.id}</h1>
+                <p>Rua:{data.logradouro}</p>
+                <p>Bairro:{data.bairro}</p>
+                <p>Cidade:{data.localidade}</p>
+                <p>Estado:{data.uf}</p>
+            </div>
+            <div>
+                <Link href="/" className="bg-blue-700 px-8 py-3 round-lg font-bold text-white text-lg
+          houer: bg-blue-800 transition-all duration-300 ease-in-out">Buscar outro CEP</Link>
+            </div>
+        </main>
+    )
+}
---
+++ src/app/zod.js
@@ -0,0 +1,10 @@
+import {z} from 'zod';
+
+export const cepFormSchema = z.object({
+    cep: z
+    .string()
+    .min(8, 'CEP deve ter 8 digitos')
+    .refine((value) => /^\d{5}\d{3}$/.test(value), {
+        message: "CEP inválido, deve ter o formato 12345678"
+    }),
+})
